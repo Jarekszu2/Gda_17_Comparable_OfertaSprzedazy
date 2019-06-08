b0cd3a1d9295253deeebd82311ea092b337b4189
unknown <jarekszu2@onet.pl>
2019-06-09 00:45:25 +0200

    09.06.2019 00:45

---
+++ src/main/java/packComparableOfertaSprzedazy/MainOfertaSprzedazy.java
@@ -0,0 +1,4 @@
+package packComparableOfertaSprzedazy;
+
+public class MainOfertaSprzedazy {
+}
---
+++ src/main/java/packComparableOfertaSprzedazy/OfertaSprzedazy.java
@@ -0,0 +1,19 @@
+package packComparableOfertaSprzedazy;
+
+/*
+Stwórz klasę OfertaSprzedaży(String nazwaProduktu, double cena) a następnie stwórz komparator który sortuje OfertySprzedaży po cenie rosnąco lub malejąco. Wskazówka : dodaj do klasy komparatora dodatkowe pole (np. typu boolean) które w zależności od wartości inaczej sortuje obiekty.
+W mainie stwórz kilka instancji klasy OfertaSprzedaży i dodaj je do Listy. Po dodaniu posortuj listę i na ekran wypisz  wynik przed sortowaniem, oraz po sortowaniu. Spróbuj również sortowania 'w drugą stronę'.
+*Stwórz parser komend. Pozwól użytkownikowi w konsoli kontrolować aplikację. Parser powinien obsługiwać komendy:
+dodaj mleko 2.3
+listuj
+sortuj rosnaco (komenda nie listuje, tylko sortuje)
+sortuj malejaco (komenda nie listuje, tylko sortuje)
+ */
+
+
+
+public class OfertaSprzedazy {
+
+    private String nazwaProduktu;
+    private double cenaProduktu;
+}
